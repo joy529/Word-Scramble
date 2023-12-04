0f793495a2101c0596dbcda5bcaf3aed414dcdbe
joy jain <joyshah204@gmial.com>
2023-12-04 19:57:31 +0530

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by JOY JAIN on 04/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
